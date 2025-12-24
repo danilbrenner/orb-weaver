302695c3a9514e2c86ead2da60cd99e05861e8c7
Danil <git@brcloud.io>
2025-12-24 08:22:11 +0200

    Kafka listener refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,30 @@
 import cats.effect.{IO, IOApp}
-import org.typelevel.log4cats.slf4j.Slf4jLogger
+import infra.KafkaListener
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.typelevel.log4cats.StructuredLogger
+import org.typelevel.log4cats.slf4j.Slf4jFactory
 
 object Main extends IOApp.Simple {
-  private val logger = Slf4jLogger.getLogger[IO]
-  
-  def run: IO[Unit] = {
-    val bootstrap = sys.env.getOrElse("KAFKA_BOOTSTRAP", "localhost:9092")
-    val topic = sys.env.getOrElse("KAFKA_TOPIC", "probe_outcomes")
+  private def processMessage(logger: StructuredLogger[IO])(key: String, value: String): IO[Unit] = {
+    logger.info(Map("message" -> value))("Processing message")
+  }
 
-    for {
-      _ <- logger.info("Starting Application...")
-      _ <- KafkaSubscriber.subscribe(topic, bootstrap)
-    } yield ()
+  override def run: IO[Unit] = {
+
+    val loggerFactory = Slf4jFactory.create[IO]
+    val baseLogger: StructuredLogger[IO] = loggerFactory.getLogger
+
+    KafkaListener(Map(
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9092",
+      ConsumerConfig.GROUP_ID_CONFIG -> "orb-weaver-group",
+      ConsumerConfig.MAX_POLL_RECORDS_CONFIG -> "10")
+    )
+      .start(
+        "probe_outcomes",
+        processMessage(
+          StructuredLogger.withContext(baseLogger)(Map("topic" -> "probe_outcomes"))
+        )
+      )
+      .handleErrorWith(t => baseLogger.error(t)(s"Kafka listener failed: ${t.getMessage}"))
   }
 }
